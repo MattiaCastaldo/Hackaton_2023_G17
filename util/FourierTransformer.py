b28ea513e6eea13b4a8889f83d87bd6692dcc927
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2023-06-30 18:14:42 +0200

    all

--- util/FourierTransformer.py
+++ util/FourierTransformer.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 from numpy.fft import fft, ifft, fftfreq
 
-
+def topk(a, k, axis=2):
+    return np.argpartition(-a, k, axis=axis)[...,:k]
 '''
     @param k: how many top features we want to consider for each region, class and (maybe) samples
     @param aggregate: different ways to aggregate the top k results:
@@ -19,18 +20,24 @@ class FourierTransformer(BaseEstimator, TransformerMixin):
         self.aggregate = aggregate
 
     def fit(self, X, y=None):
+        if self.aggregate == 'low_filter':
+            # assert type(self.k) == list, 'When predefined k must be the frequency list!'
+            self.freq_idx = list(range(self.k))
+            return self
+
         X = X.reshape(X.shape[0], 24, 2048)
-        F = FourierTransformer.power(fft(X, axis=2))
+        F = FourierTransformer.fourier_magnitude(X)[:,:,:1024]
 
         if self.aggregate == 'median':
             F_byclass = self.get_perclass_values(F, y)
-            topk_byclass = [np.median(F_y, axis=0).argsort(axis=1)[:,-self.k:] for F_y in F_byclass]
+            F_median = [np.median(F_y, axis=0) for F_y in F_byclass]
+            topk_byclass = [topk(F_y, self.k, axis=1) for F_y in F_median]
             self.freq_idx = [np.unique(np.concatenate([topk[i,:] for topk in topk_byclass])) for i in range(24)]
         elif self.aggregate == 'union':
-            topk_sample = F.argsort(axis=2)[:,:,-self.k:]
+            topk_sample = topk(F, self.k, axis=2)
             self.freq_idx = [np.unique(topk_sample[:,i,:].flatten()) for i in range(24)]
         elif self.aggregate == 'intersection' or self.aggregate == 'exclusive':
-            topk_sample = F.argsort(axis=2)[:,:,-self.k:]
+            topk_sample = topk(F, self.k, axis=2)
             topk_byclass = self.get_perclass_values(topk_sample, y)
             topk_y = [[set(topk[:,i,:].flatten()) for topk in topk_byclass] for i in range(24)]
 
@@ -49,12 +56,34 @@ class FourierTransformer(BaseEstimator, TransformerMixin):
         return y_sep
     
     def transform(self, X, y=None):
-        X = X.reshape(X.shape[0], 24, 2048)
-        F = fft(X, axis=2)
+        assert self.freq_idx is not None, 'The transformer must be fitted first!'
 
-        return np.hstack([FourierTransformer.power(F[:,i,idx]) for i,idx in enumerate(self.freq_idx)])
+        X = X.reshape(X.shape[0], 24, 2048)
+        M = FourierTransformer.fourier_magnitude(X, axis=2)
+        print([M[:,i,idx].shape for i,idx in enumerate(self.freq_idx)])
+        return np.hstack([M[:,i,idx] for i,idx in enumerate(self.freq_idx)])
     
     @staticmethod
     def power(F):
         return  2*(np.abs(F/2048))**2 
+    
+    @staticmethod
+    def fourier_magnitude(ts, axis=2):
+        return FourierTransformer.power(fft(ts, axis=axis))
+    
+    @staticmethod
+    def fourier_approximate(ts, k, axis=0):            
+        F = fft(ts, axis=axis)
+        M = FourierTransformer.power(F)
+        if type(k) is list:
+            topk_freq = k
+        else:
+            topk_freq = topk(M, k, axis=axis)
+
+        F_zeroed = np.zeros(F.shape, dtype=np.complex_)
+        F_zeroed[topk_freq] = F[topk_freq]
+
+        return np.real(ifft(F_zeroed, ts.shape[0])), topk_freq
+    
+
 
--- util/__init__.py
+++ util/__init__.py
@@ -2,3 +2,4 @@ from .input_reading import *
 from .transforms import *
 from .plots import *
 from .FourierTransformer import *
+from .OrdinalClassifier import *
